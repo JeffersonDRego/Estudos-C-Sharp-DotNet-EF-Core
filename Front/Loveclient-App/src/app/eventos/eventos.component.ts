0e92e5b470564ed2b650317da432eb3987ea12bc
Jefferson D. Rego <jeff.drsp@gmail.com>
2022-03-02 22:56:51 -0300

    6º Commit
    Angular, Interpolação, Diretiva e Binding

--- Front/Loveclient-App/src/app/eventos/eventos.component.ts
+++ Front/Loveclient-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  exibirImagem: boolean = true;
+  private _filtroLista: string = '';
+  public eventosFiltrados: any = [];
+
+
+  public get filtroLista(): string{
+    return this._filtroLista;
+  }
+  public set filtroLista(value){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+  filtrarEventos(filtrarPor: string):any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: { titulo: string; local: string; }) => evento.titulo.toLocaleLowerCase().indexOf(filtrarPor)!== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor)!== -1
+    )
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -16,10 +35,17 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  alterarImagem(){
+    this.exibirImagem =!this.exibirImagem;
+  }
+
   public getEventos(): void{
 
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response =>{
+        this.eventos = response;
+        this.eventosFiltrados= this.eventos;
+      },
       error => console.log(error)
     );
   }
