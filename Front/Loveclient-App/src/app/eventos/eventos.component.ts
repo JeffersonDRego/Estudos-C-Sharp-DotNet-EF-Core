1de7788068b4d63dce503272a807a8d35901e837
Jefferson D. Rego <jeff.drsp@gmail.com>
2022-03-14 19:33:06 -0300

    8º Commit
    Salvando para poder refazer o BD

--- Front/Loveclient-App/src/app/eventos/eventos.component.ts
+++ Front/Loveclient-App/src/app/eventos/eventos.component.ts
@@ -1,27 +1,29 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Evento } from '../models/Evento';
+import { EventoService } from '../services/evento.service';
 
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
   styleUrls: ['./eventos.component.scss']
+  // providers:[EventoService]
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any = [];
+  eventos: Evento[] = [];
+  eventosFiltrados: Evento[] = [];
+
   exibirImagem: boolean = true;
   private _filtroLista: string = '';
-  public eventosFiltrados: any = [];
 
-
-  public get filtroLista(): string{
+  get filtroLista(): string{
     return this._filtroLista;
   }
-  public set filtroLista(value){
+  set filtroLista(value){
     this._filtroLista = value;
     this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
   }
-  filtrarEventos(filtrarPor: string):any{
+  filtrarEventos(filtrarPor: string):Evento[] {
     filtrarPor = filtrarPor.toLocaleLowerCase();
     return this.eventos.filter(
       (evento: { titulo: string; local: string; }) => evento.titulo.toLocaleLowerCase().indexOf(filtrarPor)!== -1 ||
@@ -29,21 +31,20 @@ export class EventosComponent implements OnInit {
     )
   }
 
-  constructor(private http: HttpClient) { }
+  constructor(private eventoService: EventoService) { }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
     this.getEventos();
   }
 
-  alterarImagem(){
+  public alterarImagem(){
     this.exibirImagem =!this.exibirImagem;
   }
 
   public getEventos(): void{
-
-    this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response =>{
-        this.eventos = response;
+    this.eventoService.getEventos().subscribe(
+      (_eventos: Evento[]) =>{
+        this.eventos = _eventos;
         this.eventosFiltrados= this.eventos;
       },
       error => console.log(error)
